68b2b232fec8f544d5a8f812029c5bb7cc3bd969
Eric Ebulu <ericebulu@Erics-MacBook-Pro.local>
2019-11-11 13:26:47 +0200

    ch(setup): setup mongodb connection - Add connection file - Add environment variables - Add models
    [Delivers #167353304]

---
+++ server/connection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//mongoDB connection string
+const url = process.env.DB_URL;
+
+mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+  console.log('database connected!');})
+.catch(err => console.log(err));
